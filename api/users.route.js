c9d1102222f909fe8ede6e42695efb082a71d679
Skellinghoff <jarodwellinghoff@gmail.com>
2022-12-04 18:38:11 -0600

    user

--- api/users.route.js
+++ api/users.route.js
@@ -3,22 +3,11 @@ import UsersCtrl from './users.controller.js';
 
 const router = express.Router();
 
-
-function isAuthenticated(req, res, next) {
-    if (!req.session.isAuthenticated) {
-        return res.redirect('/auth/signin'); // redirect to sign-in route
-    }
-
-    next();
-}
-
-router.route('/id').get(isAuthenticated, UsersCtrl.azureGetId);
-// router.route('/find').get(UsersCtrl.apiGetUserByEmail);
+router.route('/').get(UsersCtrl.apiGetUsers);
 router.route('/username/:username').get(UsersCtrl.apiGetUserByUsername);
 router.route('/addRegisteredMarker').post(UsersCtrl.apiAddRegisteredMarker);
 router.route('/removeRegisteredMarker').post(UsersCtrl.apiRemoveRegisteredMarker);
 router.route('/addFavoriteMarker').post(UsersCtrl.apiAddFavoriteMarker);
 router.route('/removeFavoriteMarker').post(UsersCtrl.apiRemoveFavoriteMarker);
-router.route('/profile').get(isAuthenticated, UsersCtrl.azureGetProfile);
 
 export default router;
